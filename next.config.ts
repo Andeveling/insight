b17978054bc4d7d9e7cc53377d1d2a8e9ed67408
Fabian Andres Parra Sanchez <andeveling@gmail.com>
2025-12-22 12:29:03 -0500

    chore(next): set turbopack.root to project dir

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	cacheComponents: true,
 	serverExternalPackages: [
 		"@prisma/adapter-libsql",
 		"@libsql/client",
 		"@prisma/client",
 	],
+	turbopack: {
+		root: path.join(__dirname),
+	},
 };
 
 export default nextConfig;
